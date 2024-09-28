f6fa6cb4caeaee6694de91a61da94b7cae7e5a00
tataplayjiotv <saini000987098@gmail.com>
2024-09-28 20:51:04 +0530

    Update j-play.php

--- api/j-play.php
+++ api/j-play.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: http://xtv.ooo:8080/movie/kamran800/800kamran/$id");
+    header("Location: http://ktv.im:8080/Scott/Scott/$id");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
