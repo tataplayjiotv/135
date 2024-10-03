dd19c2c01a81eee60c95244f06ff2520a64d130e
tataplayjiotv <saini000987098@gmail.com>
2024-10-03 08:57:35 +0530

    Update h-play.php

--- api/h-play.php
+++ api/h-play.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: http://sansat.org:88/02014507700695/VILRJM645N662BJ/$id");
+    header("Location: http://localhost:8000/tataplay /$id");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
