ac7ca8394fb17ca5c61d4bc488b503ff5019eb63
tataplayjiotv <saini000987098@gmail.com>
2024-10-03 19:47:56 +0530

    Update h-play.php

--- api/h-play.php
+++ api/h-play.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: http://localhost:8000/tataplay /$id");
+    header("Location: http://210.89.44.180:7676/tataplay/$id/manifest.mpd");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
