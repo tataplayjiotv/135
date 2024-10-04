c02ca4954b887e5991d4c7ee8b2828dfa6f88d9d
tataplayjiotv <saini000987098@gmail.com>
2024-10-04 15:45:12 +0530

    Update and rename tp-play.php to jass.key.php

--- api/jass.key.php
+++ api/jass.key.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: http://150.242.202.110:824/tplay-hls/live/$id");
+    header("https://tplay.yuvraj49.in/tp/gflidqa8uzwA/key.php?id=$id");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
